d7dc167bdf425a5fd71ed8be167e90c9cb666cc2
smsroql12 <smsroql12@naver.com>
2020-06-12 11:08:51 +0900

    완성 버전

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace TakeOutTXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Num = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Contents = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKeywordSearch = new System.Windows.Forms.Button();
+            this.btnSaveTxT = new System.Windows.Forms.Button();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.FCountNum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FNum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FContents = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.caseCheck = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Num,
+            this.Contents});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(10, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(829, 391);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyUp);
+            // 
+            // Num
+            // 
+            this.Num.Text = "번호";
+            this.Num.Width = 40;
+            // 
+            // Contents
+            // 
+            this.Contents.Text = "내용";
+            this.Contents.Width = 625;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath.Location = new System.Drawing.Point(201, 7);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(638, 25);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKeyword.Location = new System.Drawing.Point(96, 9);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(350, 25);
+            this.txtKeyword.TabIndex = 4;
+            this.txtKeyword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtKeyword_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 9F);
+            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "찾을 단어 :";
+            // 
+            // btnKeywordSearch
+            // 
+            this.btnKeywordSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnKeywordSearch.Location = new System.Drawing.Point(600, 7);
+            this.btnKeywordSearch.Name = "btnKeywordSearch";
+            this.btnKeywordSearch.Size = new System.Drawing.Size(75, 25);
+            this.btnKeywordSearch.TabIndex = 6;
+            this.btnKeywordSearch.Text = "검색";
+            this.btnKeywordSearch.UseVisualStyleBackColor = true;
+            this.btnKeywordSearch.Click += new System.EventHandler(this.btnKeywordSearch_Click);
+            // 
+            // btnSaveTxT
+            // 
+            this.btnSaveTxT.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveTxT.Location = new System.Drawing.Point(692, 7);
+            this.btnSaveTxT.Name = "btnSaveTxT";
+            this.btnSaveTxT.Size = new System.Drawing.Size(75, 26);
+            this.btnSaveTxT.TabIndex = 7;
+            this.btnSaveTxT.Text = "저장";
+            this.btnSaveTxT.UseVisualStyleBackColor = true;
+            this.btnSaveTxT.Click += new System.EventHandler(this.btnSaveTxT_Click);
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(10, 6);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(88, 27);
+            this.btnLoadFile.TabIndex = 8;
+            this.btnLoadFile.Text = "불러오기";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FCountNum,
+            this.FNum,
+            this.FContents});
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(9, 43);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(829, 202);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            this.listView2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listView2_KeyUp);
+            // 
+            // FCountNum
+            // 
+            this.FCountNum.Text = "번호";
+            this.FCountNum.Width = 40;
+            // 
+            // FNum
+            // 
+            this.FNum.Text = "참조번호";
+            // 
+            // FContents
+            // 
+            this.FContents.Text = "내용";
+            this.FContents.Width = 625;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 9F);
+            this.label2.Location = new System.Drawing.Point(114, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "파일 경로 :";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("굴림", 10F);
+            this.richTextBox1.Location = new System.Drawing.Point(9, 254);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(829, 29);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnLoadFile);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtFilePath);
+            this.panel1.Location = new System.Drawing.Point(0, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(849, 437);
+            this.panel1.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.caseCheck);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.txtKeyword);
+            this.panel2.Controls.Add(this.listView2);
+            this.panel2.Controls.Add(this.btnKeywordSearch);
+            this.panel2.Controls.Add(this.btnSaveTxT);
+            this.panel2.Location = new System.Drawing.Point(2, 445);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(847, 296);
+            this.panel2.TabIndex = 11;
+            // 
+            // caseCheck
+            // 
+            this.caseCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.caseCheck.AutoSize = true;
+            this.caseCheck.Location = new System.Drawing.Point(461, 12);
+            this.caseCheck.Name = "caseCheck";
+            this.caseCheck.Size = new System.Drawing.Size(130, 19);
+            this.caseCheck.TabIndex = 12;
+            this.caseCheck.Text = "대/소문자 구분";
+            this.caseCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 744);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "문서검색";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Contents;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKeywordSearch;
+        private System.Windows.Forms.Button btnSaveTxT;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.ColumnHeader Num;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader FCountNum;
+        private System.Windows.Forms.ColumnHeader FContents;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ColumnHeader FNum;
+        private System.Windows.Forms.CheckBox caseCheck;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TakeOutTXT
+{
+    public partial class Form1 : Form
+    {
+        string[] lines; // 텍스트 값(줄 단위)이 복사되어 넣어질 배열
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView2.Columns[0].DisplayIndex = 0;
+            listView2.Columns[1].DisplayIndex = 1;
+            listView2.Columns[2].DisplayIndex = 2;
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "파일 불러오기";
+            ofd.Filter = "텍스트 파일 (*.txt) | *.txt; |모든 파일 (*.*) | *.*";
+            DialogResult dr = ofd.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                listView1.Items.Clear();
+                string path = ofd.FileName;
+                txtFilePath.Text = path;
+                
+                var list = new List<string>();
+                var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                using (var streamReader = new StreamReader(fileStream, Encoding.Default))
+                {
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                        list.Add(line);
+                }
+                lines = list.ToArray(); //배열 복사
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    ListViewItem lvi = new ListViewItem((i + 1).ToString());
+                    lvi.SubItems.Add(lines[i]);
+                    listView1.Items.Add(lvi);
+                }
+                richTextBox1.Text = null;
+                listView2.Items.Clear();
+            }
+        }
+
+        private void btnKeywordSearch_Click(object sender, EventArgs e)
+        {
+            if (txtFilePath.Text == null || txtFilePath.Text == "")
+            {
+                MessageBox.Show("먼저 '불러오기' 버튼을 클릭하여\n텍스트 파일을 로드 해 주세요.");
+                return;
+            }
+            else if (txtKeyword.Text == null || txtKeyword.Text == "")
+            {
+                MessageBox.Show("검색 할 키워드를 입력 해 주세요.");
+                txtKeyword.Focus();
+                return;
+            }
+            else
+            {
+                var tempLines = lines.ToArray();
+                int i = 1, counter = 1; // i -> 인덱스(개수), counter -> 참조 인덱스
+                listView2.Items.Clear();
+                foreach (var line in tempLines)
+                {
+                    if (caseCheck.Checked == true)
+                    {
+                        if (line.Contains(txtKeyword.Text, StringComparison.CurrentCulture))
+                        {
+                            ListViewItem lvi = new ListViewItem(i.ToString()); // 인덱스
+                            lvi.SubItems.Add(counter.ToString()); // 참조 인덱스
+                            lvi.SubItems.Add(line); // 내용
+                            listView2.Items.Add(lvi);
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        if (line.Contains(txtKeyword.Text, StringComparison.OrdinalIgnoreCase))
+                        {
+                            ListViewItem lvi = new ListViewItem(i.ToString()); // 인덱스
+                            lvi.SubItems.Add(counter.ToString()); // 참조 인덱스
+                            lvi.SubItems.Add(line); // 내용
+                            listView2.Items.Add(lvi);
+                            i++;
+                        }
+                    }
+                    ++counter;
+                }
+            }
+        }
+        
+        private void btnSaveTxT_Click(object sender, EventArgs e)
+        {
+            List<string> txtExport = new List<string>();
+            for (int i = 0; i < listView2.Items.Count; ++i) 
+                txtExport.Add(listView2.Items[i].SubItems[1].Text + " >>> " + listView2.Items[i].SubItems[2].Text);
+            
+            string fileName;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "저장 경로를 지정 하세요.";
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.Filter = "텍스트 파일 (*.txt) | *.txt |모든 파일 (*.*) | *.*";
+            saveFileDialog.FileName = "textFile";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog.FileName;
+                try
+                {
+                    File.WriteAllLines(fileName, txtExport);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else   return;
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                if (listView2.SelectedItems[0].SubItems[0] != null)
+                {
+                    richTextBox1.Text = null;
+                    richTextBox1.Text = listView2.SelectedItems[0].SubItems[2].Text;
+                    richTextBox1.SelectionStart = 0;
+                    richTextBox1.SelectAll();
+                    richTextBox1.SelectionBackColor = Color.White;
+                }
+                else    MessageBox.Show("일치하는 단어를 찾을 수 없습니다.");
+                colorMatchWords(); //일치하는 단어 배경색 변경
+            }
+        }
+
+        private void colorMatchWords()
+        {
+            int index = 0;
+            while (index < richTextBox1.Text.LastIndexOf(txtKeyword.Text))
+            {
+                richTextBox1.Find(txtKeyword.Text, index, richTextBox1.TextLength, RichTextBoxFinds.None);
+                richTextBox1.SelectionBackColor = Color.GreenYellow;
+                index = richTextBox1.Text.IndexOf(txtKeyword.Text, index) + 1;
+            }
+
+            string[] words = txtKeyword.Text.Split(',');
+            foreach (string word in words)
+            {
+                int startindex = 0;
+                while (startindex < richTextBox1.TextLength)
+                {
+                    int wordstartIndex = richTextBox1.Find(word, startindex, RichTextBoxFinds.None);
+                    if (wordstartIndex != -1)
+                    {
+                        richTextBox1.SelectionStart = wordstartIndex;
+                        richTextBox1.SelectionLength = word.Length;
+                        richTextBox1.SelectionBackColor = Color.GreenYellow;
+                    }
+                    else   break;
+                    startindex += wordstartIndex + word.Length;
+                }
+            }
+        }
+
+        private void CopySelectedValuesToClipboard(ListView lv, int index)
+        {
+            var builder = new StringBuilder();
+            foreach (ListViewItem item in lv.SelectedItems)
+                builder.AppendLine(item.SubItems[index].Text);
+            Clipboard.SetText(builder.ToString());
+        }
+
+        private void txtKeyword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)    btnKeywordSearch_Click(this, new EventArgs()); //txtKeyword 포커스 중 Enter키 입력 시 btnKeywordSearch 이벤트 (검색 버튼 클릭 이벤트) 실행
+        }
+
+        private void listView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (sender != listView1) return;
+            if (e.Control && e.KeyCode == Keys.C)
+                CopySelectedValuesToClipboard(listView1, 1);
+        }
+
+        private void listView2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (sender != listView2) return;
+            if (e.Control && e.KeyCode == Keys.C)
+                CopySelectedValuesToClipboard(listView2, 2);
+        }
+    }
+}
